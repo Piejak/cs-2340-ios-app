7f308601c83ad78ae70616698fa9131012a93669
李辉 <lihui@lihuis-MacBook-Air.local>
2017-02-26 01:53:47 -0500

    Create login ViewControl and Register ViewControl

---
+++ WaterReport/RegisterViewController.swift
@@ -0,0 +1,66 @@
+//
+//  RegisterViewController.swift
+//  WaterReport
+//
+//  Created by HUI Li on 2/26/17.
+//  Copyright © 2017 Brian Piejak. All rights reserved.
+//
+
+import UIKit
+import Firebase
+
+class RegisterViewController: UIViewController {
+
+    @IBOutlet weak var Email: UITextField!
+    
+    
+    @IBOutlet weak var Password: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    @IBAction func CreateAccount(_ sender: AnyObject) {
+        FIRAuth.auth()?.createUser(withEmail: Email.text!, password: Password.text!, completion: {
+            user, error in
+            
+            if error != nil {
+                self.login()
+                print("Wrong")
+            }
+            else {
+                print("User Created")
+                self.login()
+            }
+        })
+    }
+    
+    
+    func login() {
+        FIRAuth.auth()?.signIn(withEmail:Email.text!, password: Password.text!, completion: {
+            user, error in
+            
+            if error != nil {
+                print("Incorrect")
+            }
+        })
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
