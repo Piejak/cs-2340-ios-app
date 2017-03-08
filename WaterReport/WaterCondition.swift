529b966f4cd2d70176a62856d4c8bafe066ef774
李辉 <lihui@lihuis-MacBook-Air.local>
2017-03-08 18:02:11 -0500

    add the WaterCondition enum

---
+++ WaterReport/WaterCondition.swift
@@ -0,0 +1,17 @@
+//
+//  WaterCondition.swift
+//  WaterReport
+//
+//  Created by Hui Li on 3/8/17.
+//  Copyright © 2017 Brian Piejak. All rights reserved.
+//
+
+import Foundation
+
+
+enum WaterCondition : String {
+    case Waste = "Waste"
+    case TreatableClear = "Treatable-Clear"
+    case TreatableMuddy = "Treatable-Muddy"
+    case Potable = "Potable"
+}
